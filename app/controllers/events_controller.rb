a8ca2b12cafdf463680579e8df57265891ae4f2b
EricPickup <pickup@uwindsor.ca>
2019-02-11 20:44:08 -0800

    Fixed merge conflict, added delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,11 @@
 class EventsController < ApplicationController
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to events_path
+  end
+
   def index
     @events = Event.all
   end
