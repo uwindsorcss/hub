06a8ac09f1264463f053d93c4e3ce1b3780d86b6
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2019-02-20 14:33:04 -0500

    Added footer and solved sidebar issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 Rails.application.config.assets.precompile += %w( materialize.min.css )
 Rails.application.config.assets.precompile += %w( materialize.min.js )
+Rails.application.config.assets.precompile += %w( sidenav.css )
