217ee619fc7eb820258e740aa849f56a917705c1
EricPickup <pickup@uwindsor.ca>
2019-02-19 20:54:21 -0800

    Built Past Events section

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,27 +1,26 @@
 class RegistrationController < ApplicationController
   def new
     user_id = params[:user_id].to_i
-    event_id = params[:event_id].to_i
-    if current_user.id == user_id && !(user_already_registered? user_id, event_id)
+    event = Event.find(params[:event_id])
+    if current_user.id == user_id && !(user_already_registered? user_id, event.id) && event.date.future?
       @registration = Registration.new(
         user_id: user_id,
-        event_id: event_id
+        event_id: event.id
       )
-      @event = Event.find(event_id)
-      if (1 + @registration.guests.size) > spots_remaining(@event)
-        redirect_to event_path(event_id), :flash => { :error => "There are #{spots_remaining(@event)} spot(s) available!" }
+      if (1 + @registration.guests.size) > spots_remaining(event)
+        redirect_to event_path(event), :flash => { :error => "There are #{spots_remaining(event)} spot(s) available!" }
       else
         @registration.save
-        redirect_to event_path(event_id), :flash => { :success => "Successfully registered for event!" }
+        redirect_to event_path(event), :flash => { :success => "Successfully registered for event!" }
       end
     else
-      redirect_to event_path(event_id), :flash => { :error => "Something went wrong." }
+      redirect_to event_path(event), :flash => { :error => "Something went wrong." }
     end
   end
 
   def destroy
     @registration = Registration.find(params[:registration_id])
-    if current_user == @registration.user
+    if current_user == @registration.user && @registration.event.date.future?
       @registration.destroy
     end
     redirect_to event_path(params[:event_id])
