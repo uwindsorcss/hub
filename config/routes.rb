4ae09a592fa23550e242baa4879acba93a4fe753
Eric Pickup <pickup@uwindsor.ca>
2019-05-03 13:52:57 -0400

    Moved guest list to event page

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,7 @@ Rails.application.routes.draw do
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html	  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "events#index"
 
-  resources :events do
-    get :guests, on: :member
-  end
+  resources :events
 
   resources :registration
 
