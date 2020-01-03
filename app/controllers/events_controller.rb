af773c63beb2e66d20e4e57060a763bf8664cf7e
Eric Pickup <pickup@uwindsor.ca>
2020-01-03 14:14:09 -0500

    Remove unused controller action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -41,13 +41,6 @@ class EventsController < ApplicationController
     end
   end
 
-  def guests
-    @event = Event.find(params[:id])
-    unless current_user&.is_admin?
-      display_404
-    end
-  end
-
   def create
     @event = Event.new(event_params)
     if current_user&.is_admin?
