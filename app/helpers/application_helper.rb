7b4e6f84f7d6e82abc417790871f745ac2b220f1
Eric Pickup <epickup98@gmail.com>
2020-01-07 10:41:13 -0500

    Added custom metadate for event pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -48,4 +48,16 @@ module ApplicationHelper
       "<a href=\"#{link}\"><img src=\"#{link}\" class=\"img-responsive\" alt=\"#{alt_text}\" width=\"80%\"></a>"
     end
   end
+
+  def title(text)
+    content_for :title, text
+  end
+
+  def meta_tag(tag, text)
+    content_for :"meta_#{tag}", text
+  end
+
+  def yield_meta_tag(tag, default_text='')
+    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+  end
 end
