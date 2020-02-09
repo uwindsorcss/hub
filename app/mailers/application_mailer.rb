33f0c7f5fe8e589738bacd2d3b9cf4d87a126b3e
Eric Pickup <pickup@uwindsor.ca>
2020-02-09 18:57:58 -0500

    Added waitlist feature

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  add_template_helper(ApplicationHelper)
   default from: ENV['GMAIL_USERNAME']
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: ENV['GMAIL_USERNAME']
+  default from: "Computer Science Society <#{ENV['GMAIL_USERNAME']}>"
 
-  def welcome_email
+  def waitlist_email
     @user = params[:user]
+    @event = params[:event]
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: "You're in! #{@event.title}")
   end
 end
