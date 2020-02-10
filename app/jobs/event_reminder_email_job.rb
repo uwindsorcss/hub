d5a9d7c7429b7f0e851b321fbb25f0644c8eb1d6
Eric Pickup <pickup@uwindsor.ca>
2020-02-09 23:39:31 -0500

    Switch to deliver now

--- app/jobs/event_reminder_email_job.rb
+++ app/jobs/event_reminder_email_job.rb
@@ -4,7 +4,7 @@ class EventReminderEmailJob < ApplicationJob
   def perform(event)
     if event&.registration_enabled
       email_list = event.registered_users.each_with_object([]) { |u, email_list| email_list << u.email }
-      UserMailer.with(bcc_list: email_list, event: event).event_reminder_email.deliver_later
+      UserMailer.with(bcc_list: email_list, event: event).event_reminder_email.deliver_now
     end
   end
 end
