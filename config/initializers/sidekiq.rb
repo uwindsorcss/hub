6697504e4b693ce662fb329a090dbf8851bcd73a
Eric Pickup <pickup@uwindsor.ca>
2020-02-27 11:51:58 -0500

    Change sidekiq port

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6380/0' }
+  config.redis = { url: 'redis://localhost:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6380/0' }
+  config.redis = { url: 'redis://localhost:6379/0' }
 end
