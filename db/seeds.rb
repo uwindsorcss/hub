443187e87e312d5ec16967c6c9d91bfa8528faba
amanpatel123 <patel1wq@uwindsor.ca>
2020-03-17 17:53:54 -0400

    seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 10.times do |n|
-#     Event.create!(title:  "test#{n}",
-#                  description: "This is description #{n}", 
-#                  location: "location: #{n}",
-#                  capacity: "12",
-#                  registration_enabled: true,
-#                  start_date: Time.zone.now, 
-#                  end_date: 2.days.from_now)
-#   end
+10.times do |n|
+    Event.create!(title:  "test#{n}",
+                 description: "This is description #{n}", 
+                 location: "location: #{n}",
+                 capacity: "12",
+                 registration_enabled: true,
+                 start_date: Time.zone.now, 
+                 end_date: 2.days.from_now)
+  end
 
   13.times do |n|
     Event.create!(title:  "Test#{n}",
