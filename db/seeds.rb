17831a54ae2aab99f068ea8522b557d6d3fca049
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2020-03-20 14:37:10 -0400

    Add copy emails to clipboard button and seeds (#145)
    * copy to clipboard button
    
    * db seeds
    
    Co-authored-by: Harshdip Deogan <hdeogan@deloitte.ca>

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Upcoming events
 10.times do |n|
     Event.create!(title:  "test#{n}",
                  description: "This is description #{n}", 
@@ -16,12 +17,29 @@
                  end_date: 2.days.from_now)
   end
 
-  13.times do |n|
-    Event.create!(title:  "Test#{n}",
-                 description: "This is description #{n}", 
-                 location: "location: #{n}",
-                 capacity: "12",
-                 registration_enabled: true,
-                 start_date: Time.zone.now - 5.days, 
-                 end_date: Time.zone.now - 2.days)
-  end
+# Past event
+13.times do |n|
+  Event.create!(title:  "Test#{n}",
+                description: "This is description #{n}", 
+                location: "location: #{n}",
+                capacity: "12",
+                registration_enabled: true,
+                start_date: Time.zone.now - 5.days, 
+                end_date: Time.zone.now - 2.days)
+end
+
+
+# User Create
+10.times do |n|
+  User.create!(id: n+2,
+                email: "#{n+2}@uwindsor.ca",
+                name: "User #{n+1}",
+                role: "guest")
+end
+
+# Let random user register existing event
+1.times do |n|
+  Registration.create!(user_id: n+2,
+                      event_id: 1,
+                      waitlisted: false)
+end
