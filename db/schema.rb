43b09a2f6a982e5eb97986b8a59ff05cc03706b4
amanpatel123 <patel1wq@uwindsor.ca>
2020-05-09 13:41:44 -0400

    changed name from newsPost to Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200501023142) do
+ActiveRecord::Schema.define(version: 20200509173440) do
 
   create_table "discord_users", force: :cascade do |t|
     t.integer "discord_uid", limit: 8
@@ -50,9 +50,9 @@ ActiveRecord::Schema.define(version: 20200501023142) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "news_posts", force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "description"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
