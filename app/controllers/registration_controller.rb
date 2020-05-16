ce3e1cc11d32f724347f81287b22ec619a0e0ae2
Aman Patel <46072181+amanpatel123@users.noreply.github.com>
2020-05-16 16:14:32 -0400

    removed if-else (#174)

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -11,22 +11,18 @@ class RegistrationController < ApplicationController
       if (1 + @registration.guests.size) > event.spots_remaining
         @registration.assign_attributes(waitlisted: true)
         @registration.save
-        redirect_to event_path(event), :flash => { :success => "You've been added to the waitlist for this event! You will receive an email if you make it off the list. Position in the waitlist: #{@registration.position_in_waitlist}" }
+        redirect_to event_path(event), flash: { success: "You've been added to the waitlist for this event! You will receive an email if you make it off the list. Position in the waitlist: #{@registration.position_in_waitlist}" }
       else
         @registration.save
-        redirect_to event_path(event), :flash => { :success => "Successfully registered for event!" }
+        redirect_to event_path(event), flash: { success: "Successfully registered for event!" }
       end
     else
-      redirect_to event_path(event), :flash => { :error => "Something went wrong." }
+      redirect_to event_path(event), flash: { error: "Something went wrong." }
     end
   end
 
   def destroy
-    if params[:registration_id].present?
-      @registration = Registration.find(params[:registration_id])
-    else
-      @registration = Registration.find_by(user_id: params[:guest_id], event_id: params[:id])
-    end
+    @registration = Registration.find_by(user_id: params[:user_id], event_id: params[:id])
     
     if ((current_user == @registration.user) || current_user.is_admin?) && @registration.event.start_date.future?
       user_waitlisted = @registration.waitlisted
