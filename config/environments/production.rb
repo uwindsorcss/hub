d321fa1d284bd27f0b9adf31d82ba67a353c57be
= <=>
2020-07-13 19:26:17 -0400

    found the culprit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,11 +17,12 @@ Rails.application.configure do
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
-
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  config.serve_static_files = false
+  config.assets.compress = true
+  config.assets.digest = true
+  
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
