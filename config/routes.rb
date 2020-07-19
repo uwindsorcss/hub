e77459287b60a7fdd387dd7006ced36b882fb6cf
Aman Patel <46072181+amanpatel123@users.noreply.github.com>
2020-07-19 14:19:45 -0400

    Scavanger Hunt SPA| Initial setup (#200)
    Co-authored-by: = <=>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'events/index'
-
   get 'session/destroy', as: 'logout'
   get 'session/destroy_discord', as: 'discord_logout'
 
@@ -10,13 +9,15 @@ Rails.application.routes.draw do
 
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
-
   resources :events
-
   resources :markdown_pages, only: [:index, :edit, :update, :new, :create]
   resources :posts, only: [:index, :edit, :update, :new, :create]
 
   resources :registration
+  
+  scope module: 'science_society' do
+    resources :scavenger_hunt, path: '/hunt', only: %i[index]
+  end
 
   get '/discord', to: 'markdown_pages#discord', as: 'discord_path'
   get '/guide', to: 'markdown_pages#guide', as: 'guide'
