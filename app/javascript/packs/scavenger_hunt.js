bddb55ac3e8d9f3a750df0b923d83266c12a5bf2
Aman Patel <46072181+amanpatel123@users.noreply.github.com>
2020-07-23 01:26:32 -0400

    fix prod error (#203)
    Co-authored-by: = <=>

--- app/javascript/packs/scavenger_hunt.js
+++ app/javascript/packs/scavenger_hunt.js
@@ -1,6 +1,8 @@
 import ReactRailsUJS from "react_ujs";
+import "../styles/scavenger_hunt.scss";
+
 console.log('SciSoc scavanger hunt app loaded');
 
-const componentRequireContext = require.context("scavenger_hunt", true);
+const componentRequireContext = require.context("scavenger_hunt", false);
 
 ReactRailsUJS.useContext(componentRequireContext);
