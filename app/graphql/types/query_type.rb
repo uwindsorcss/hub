1cc66ff7ae47f7940591133176c537dd12559fb1
= <=>
2020-07-23 23:20:58 -0400

    User Type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,10 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :users, [Types::UserType], null: false, description: "returns all the users"
+
+    def users
+      User.all
     end
   end
 end
