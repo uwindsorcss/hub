36580a1175dfcde88bdd5496618965c1530366a1
= <=>
2020-07-24 00:24:38 -0400

    Webpack performace

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const miniCssPlugin = new MiniCssExtractPlugin({
+  filename: "css/[name].css",
+  chunkFilename: "css/[id].css",
+});
+environment.plugins.append("MiniCssExtract", miniCssPlugin);
+
 module.exports = environment.toWebpackConfig()
