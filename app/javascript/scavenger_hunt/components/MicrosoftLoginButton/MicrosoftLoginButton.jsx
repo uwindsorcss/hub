f775baca211eb871c5a5be1db0dcaee0d143d762
= <=>
2020-08-01 13:57:04 -0400

    hide google client

--- app/javascript/scavenger_hunt/components/MicrosoftLoginButton/MicrosoftLoginButton.jsx
+++ app/javascript/scavenger_hunt/components/MicrosoftLoginButton/MicrosoftLoginButton.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import MicrosoftLogin from "react-microsoft-login";
-
-const SCAVENGER_HUNT_CLIENT_ID = '819d4ee7-63c4-4df1-aa7d-730b05e1fe7b';
  
 const MicrosoftLoginButton = (props) => {
   const authHandler = (err, data) => {
@@ -9,7 +7,7 @@ const MicrosoftLoginButton = (props) => {
   };
  
   return (
-    <MicrosoftLogin clientId={SCAVENGER_HUNT_CLIENT_ID} authCallback={authHandler} />
+    <MicrosoftLogin clientId={process.env.SCAVENGER_HUNT_CLIENT_ID} authCallback={authHandler} />
   );
 };
 
