3dac20a24565c620394ed1411f2e9f9c0f506072
prairir <me@ryanprairie.com>
2020-08-09 17:36:32 -0400

    make a simple homepage and simple progress componenet

--- app/javascript/scavenger_hunt/components/Progress/Progress.jsx
+++ app/javascript/scavenger_hunt/components/Progress/Progress.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepButton from '@material-ui/core/StepButton';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepConnector from '@material-ui/core/StepConnector';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import './Progress.scss'
 
-const Progress = () => (
-  <div>
-    <p> progress placeholder </p>
-  </div>
-)
+
+const StyleConnector = withStyles({
+  alternativeLabel: {
+    top: 15,
+  },
+  active: {
+    '& $line': {
+      backgroundColor: '#6aad5e',
+    },
+  },
+  completed: {
+    '& $line': {
+      backgroundColor: '#6aad5e',
+    },
+  },
+  line: {
+    height: 4,
+    backgroundColor: '#b6b6ba',
+  }
+})(StepConnector);
+
+const useStyles = makeStyles({
+  root: {
+    width: 35,
+    height: 35,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '&$completed': {
+      color: '#6aad5e',
+    },
+    '&$active': {
+      color: '#6aad5e',
+    },
+  },
+  active: {},
+  completed: {},
+})
+
+const Progress = () => {
+  
+  //no need to have labels and waste space
+  const problemNum = 10;
+  const steps = Array(problemNum).fill();
+  const data = 0;
+
+  const styles = useStyles();
+
+  return (
+    <div className="progressRoot">
+      <Stepper alternativeLabel activeStep={data} connector={<StyleConnector />}>
+        {steps.map((_, index) => (
+          <Step key={index}>
+            <StepButton>
+              <StepLabel
+                StepIconProps={{
+                  classes: {
+                    root: styles.root,
+                    completed: styles.completed,
+                    active: styles.active,
+                  }
+                }}
+              />
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
+    </div>
+  );
+}
 
 export {Progress};
