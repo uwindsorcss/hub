e137a7b7bb123bee96a3e7fa9b8cc5799639b07e
Aman Patel <46072181+amanpatel123@users.noreply.github.com>
2020-08-16 19:26:57 -0400

    Restricted pages (#223)
    * PlayArena
    
    * minor css modification
    
    * routing on rails side
    
    * should be the last line of routes.rb
    
    * Restricting play arena to only Logged in user
    
    Co-authored-by: = <=>

--- app/javascript/scavenger_hunt/index.js
+++ app/javascript/scavenger_hunt/index.js
@@ -1,1 +1,1 @@
-export { App } from './App.jsx'
+export { App } from "./App";
--- app/javascript/scavenger_hunt/scenes/PlayArena/PlayArena.jsx
+++ app/javascript/scavenger_hunt/scenes/PlayArena/PlayArena.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useCurrentUserQuery } from '../../data/queries';
+
 import { Progress } from '../../components/Progress'
 import { Navigation } from './Navigation';
 import { MainContent } from './MainContent';
 import './PlayArena.scss'
 
-const PlayArena = () => {
+const PlayArena = (props) => {
+  const { loading: queryLoading } = useCurrentUserQuery();
+
+  if (queryLoading) {
+    return(
+      <h3>Yet Loading...</h3>
+    )
+  }
+
   return (
     <div className="main">        
       <Progress />
