27f0fadcbc2128a91558f79c23eb7288213ebd4b
= <=>
2020-08-25 18:44:28 -0400

    Take the user input on submit

--- app/javascript/scavenger_hunt/scenes/PlayArena/MainContent/MainContent.jsx
+++ app/javascript/scavenger_hunt/scenes/PlayArena/MainContent/MainContent.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainContent.scss';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
 import { Clues } from '../../../data/staticData/clues';
+import {
+  Grid,
+  InputLabel,
+  TextField,
+  FormHelperText
+} from '@material-ui/core';
+import { Button } from 'react-bootstrap';
 
 
 const MainContent = ({ progress, clueId }) => {
+  const [answer, setAnswer] = useState("");
+
   const getClue = () => {
     let index = clueId - 1
-    if ((clueId - 1)  > progress) {
+    if ((clueId - 1) > progress) {
       window.location.href = `/hunt/play/${progress + 1}`
       index = progress
     }
@@ -18,27 +25,54 @@ const MainContent = ({ progress, clueId }) => {
 
   const renderQuestion = () => {
     const clue = getClue();
-    return (<h3 key={clueId} className="question">{clue.question}</h3>);
+    return ( 
+      <h3>{clue.question}</h3> 
+      );
   }
   
+  const renderHint = () => {
+    const clue = getClue();
+    return (
+      <strong>{clue.Hint}</strong>
+      );
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Answer Submitted is:", answer);
+  }
+
+  const handleChange = (event) => {
+    setAnswer(event.target.value);
+  }
+
   return (
     <Grid container spacing={3}>
-      <Grid item xs={2}>
-      </Grid>
+      <Grid item xs={2} />
       <Grid item xs={8}>
-        <div className="maincontent__container">
-          <div className="maincontent__items">
-            <form className="maincontent__form">
-              {renderQuestion()}
-              <div className="maincontent__textfield">
-                <TextField id="standard-basic" label="Answer" />
+        <div className="mainform__container">
+          <div className="mainform__items">
+            <form className="maincontent__form" onSubmit={handleSubmit} >
+              <InputLabel>
+                {renderQuestion()}
+              </InputLabel>
+              <div className="mainform__textfield">
+                <TextField required 
+                  id="question" 
+                  label="Answer" 
+                  variant="outlined"
+                  aria-describedby="Write your answer here" 
+                  value={answer} 
+                  onChange={handleChange}
+                />
+                <FormHelperText id="my-helper-text">{renderHint()}</FormHelperText>
               </div>
+              <Button variant="primary" type="submit">Submit</Button>
             </form>
           </div>
         </div>
       </Grid>
-      <Grid item xs={2}>
-      </Grid>
+      <Grid item xs={2} />
     </Grid>
   )
 };
