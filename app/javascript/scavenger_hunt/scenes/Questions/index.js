d8a8974808c671562f0cea2962e41ade1a72640f
Prakort <leanprakort@gmail.com>
2020-09-04 22:26:11 -0400

    add quzzle 5

--- app/javascript/scavenger_hunt/scenes/Questions/index.js
+++ app/javascript/scavenger_hunt/scenes/Questions/index.js
@@ -2,3 +2,4 @@ export { QuestionOne } from './QuestionOne';
 export { QuestionTwo } from './QuestionTwo';
 export { QuestionThree } from './QuestionThree';
 export { QuestionFour } from './QuestionFour';
+export { QuestionFive } from './QuestionFive';
