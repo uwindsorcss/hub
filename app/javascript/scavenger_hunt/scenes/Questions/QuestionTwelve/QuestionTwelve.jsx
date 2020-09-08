79c06545bf00b1cf5fc02b217e98adf156ac7616
Prakort <leanprakort@gmail.com>
2020-09-08 11:22:08 -0400

    disable and hide button for the correct answer

--- app/javascript/scavenger_hunt/scenes/Questions/QuestionTwelve/QuestionTwelve.jsx
+++ app/javascript/scavenger_hunt/scenes/Questions/QuestionTwelve/QuestionTwelve.jsx
@@ -73,7 +73,7 @@ const  QuestionTwelve = ({ progress, setActiveStep, completed, setCompleted  }) 
           </div>
           <Grid container justify="center" alignItems="center">
           {
-            submitted && toggle &&
+            (completed[progress].isCompleted || (submitted && toggle)) &&
               <CheckCircleOutlineIcon style={{ color: 'green', width: 50, height: 50}}/>
           }
           {
@@ -83,7 +83,7 @@ const  QuestionTwelve = ({ progress, setActiveStep, completed, setCompleted  }) 
                  
           </Grid>
           {
-           !toggle &&
+           (!toggle && !completed[progress].isCompleted)  &&
             <div className="center-text">
               <Button 
                 variant="primary" 
