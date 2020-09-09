f1c1c665cdf7b14adeb66ca60a36471b5dc53441
Prakort <leanprakort@gmail.com>
2020-09-09 19:52:24 -0400

    fix Q3

--- app/javascript/scavenger_hunt/scenes/Questions/QuestionThree/QuestionThree.jsx
+++ app/javascript/scavenger_hunt/scenes/Questions/QuestionThree/QuestionThree.jsx
@@ -21,13 +21,39 @@ const  QuestionThree = ({progress, setActiveStep, completed, setCompleted }) => 
   const [submitted, setSubmitted] = useState(false);
   const ans = Clues[2].answers;
 
+  const { data: getUserAnswerQueryData, loading: getUserAnswerQueryLoading } = useGetUserAnswerQuery({
+    variables: {
+      question_number: 3
+    }
+  });
+
+  const [saveUserAnswer, { loading: mutationLoading }] = useSaveUserAnswerMutation();
+
+  useEffect(() => {
+    if(!getUserAnswerQueryLoading) {
+      let persisted_user_answer = getUserAnswerQueryData.currentUser.answerTo;
+      if(persisted_user_answer){
+        updateCompleted();
+        setDateOne(persisted_user_answer.split(', ')[0]);
+        setDateTwo(persisted_user_answer.split(', ')[1]);
+      }
+    }
+  });
+
+  const updateCompleted = () => {
+    const newCompleted = completed;
+    newCompleted[progress].score = 2;
+    newCompleted[progress].isCompleted = true;
+    setCompleted(newCompleted);
+  }
   const handleSubmit = (event) => {
 
     event.preventDefault();
     setSubmitted(true);
     setLoading(true);
-    const one = check(DateOne.toString(), ans[0].toString());
-    const two = check(DateTwo.toString(), ans[1].toString());
+    const one =   ans.includes(DateOne.toString());
+    const two =  ans.includes(DateTwo.toString()) && DateOne !== DateTwo;
+    
     const newCompleted = completed;
     newCompleted[progress].score = 0;
     
